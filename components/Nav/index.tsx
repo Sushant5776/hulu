c987eece2e30595f2b10854149d8636036792c97
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-03-13 23:23:09 +0530

    Render List of Movies

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import requests, { Request } from 'requests'
+import requests, { Request } from '@/utils/requests'
 
 const Nav = () => {
 	const router = useRouter()
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Header from '@/components/Header'
 import Nav from '@/components/Nav'
-import type { NextPage } from 'next'
+import Results from '@/components/Results'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
+import requests from '@/utils/requests'
+import { Result } from '@/types/results'
 
-const Home: NextPage = () => {
+const Home: NextPage<{ results: Result[] }> = ({ results }) => {
 	return (
 		<div>
 			<Head>
@@ -17,8 +20,22 @@ const Home: NextPage = () => {
 			{/* Nav */}
 			<Nav />
 			{/* Results */}
+			<Results results={results} />
 		</div>
 	)
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const genre = context.query.genre
+	const request: { results: Result[] } = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre as string].url || requests.fetchTrending.url
+		}`
+	).then((res) => res.json())
+
+	return {
+		props: { results: request.results },
+	}
+}
